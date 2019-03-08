a0e7283c72a56f026bcfb2f4af8098d1cb749a27
elrizal <erinrizal@Erins-MacBook-Pro.local>
2019-03-08 12:02:31 -0500

    pj

---
+++ build/precache-manifest.9ac5e336d5163feb1556b324a3b6d247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6ba1c9dd0ce82561f2c",
+    "url": "/clickgame/static/css/main.964b69bf.chunk.css"
+  },
+  {
+    "revision": "f6ba1c9dd0ce82561f2c",
+    "url": "/clickgame/static/js/main.f8587061.chunk.js"
+  },
+  {
+    "revision": "0f98500e31e84fb21f0a",
+    "url": "/clickgame/static/js/runtime~main.619d8fb6.js"
+  },
+  {
+    "revision": "419c0b9a7daf8c5f91ae",
+    "url": "/clickgame/static/js/2.fe6d0b20.chunk.js"
+  },
+  {
+    "revision": "d716f516177d532748f82bc120042edc",
+    "url": "/clickgame/index.html"
+  }
+];
