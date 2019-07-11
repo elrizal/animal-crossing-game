a292422ff0a62911d643cffb09666917b258ec34
elrizal <erin@lrizal.com>
2019-07-10 23:07:27 -0400

    audit

--- build/precache-manifest.203b916d1cc1f042f3c22cedab78d4e4.js
+++ build/precache-manifest.203b916d1cc1f042f3c22cedab78d4e4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "2d99440ec6397db9983b",
-    "url": "/clickgame/static/css/main.a6f7461a.chunk.css"
+    "revision": "7b2600ffbc69d33ff5a2",
+    "url": "/clickgame/static/css/main.58d8baaa.chunk.css"
   },
   {
-    "revision": "2d99440ec6397db9983b",
-    "url": "/clickgame/static/js/main.2b788142.chunk.js"
+    "revision": "7b2600ffbc69d33ff5a2",
+    "url": "/clickgame/static/js/main.b7329153.chunk.js"
   },
   {
     "revision": "0f98500e31e84fb21f0a",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/clickgame/static/js/2.fe6d0b20.chunk.js"
   },
   {
-    "revision": "5753629c10f0ad55aea34bf5be39f649",
+    "revision": "7d286c868e66f30423b38f9174205f84",
     "url": "/clickgame/index.html"
   }
 ];
