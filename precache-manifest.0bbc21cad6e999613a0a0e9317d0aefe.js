775c39031ec36e5d67ed0e565db7d9ce3ff92cd1
elrizal <erin@lrizal.com>
2020-04-19 23:40:12 -0400

    Updates

---
+++ precache-manifest.0bbc21cad6e999613a0a0e9317d0aefe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dcc7b4b73d58fcb23ad6e0a03da49aa",
+    "url": "/clickgame/index.html"
+  },
+  {
+    "revision": "0f68115d2e86d8eef67e",
+    "url": "/clickgame/static/css/main.7dae718d.chunk.css"
+  },
+  {
+    "revision": "7052aed62b37e658a3d9",
+    "url": "/clickgame/static/js/2.fa4d41a1.chunk.js"
+  },
+  {
+    "revision": "0f68115d2e86d8eef67e",
+    "url": "/clickgame/static/js/main.37d8e93e.chunk.js"
+  },
+  {
+    "revision": "550c79de082ae6f7d9ff",
+    "url": "/clickgame/static/js/runtime~main.5bc58040.js"
+  }
+]);
