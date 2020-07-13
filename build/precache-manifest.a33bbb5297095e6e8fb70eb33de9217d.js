be0e636477f431658e53fb5823192305a60aa561
elrizal <erin@lrizal.com>
2020-07-12 23:49:54 -0400

    dep

---
+++ build/precache-manifest.a33bbb5297095e6e8fb70eb33de9217d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfee21ad284cb32f85eaf0510597fbcb",
+    "url": "/animal-crossing-game/index.html"
+  },
+  {
+    "revision": "9473675a52f34544c3cc",
+    "url": "/animal-crossing-game/static/css/main.06eda0db.chunk.css"
+  },
+  {
+    "revision": "d365747c571431857afd",
+    "url": "/animal-crossing-game/static/js/2.04a69dea.chunk.js"
+  },
+  {
+    "revision": "14f157ff962d9b8ca8ad2f18b4ea0d6a",
+    "url": "/animal-crossing-game/static/js/2.04a69dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9473675a52f34544c3cc",
+    "url": "/animal-crossing-game/static/js/main.a7bc6e07.chunk.js"
+  },
+  {
+    "revision": "30366516ae1d16468d6f",
+    "url": "/animal-crossing-game/static/js/runtime-main.9382e5ef.js"
+  }
+]);
